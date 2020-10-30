be9abb0d4e740989a1c67521d25e6eaa5e0ce7b3
Diana <you@example.com>
2020-10-30 02:47:29 +0100

    first pie chart on dashboard

--- DashAPP/app.py
+++ DashAPP/app.py
@@ -1,59 +1,72 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
-import datetime
-from dateutil.relativedelta import relativedelta
-from iexfinance.stocks import get_historical_data
 import plotly.graph_objs as go
-from helper_data import options_list
-from dash.dependencies import Input, Output, State
+import pandas as pd
+import numpy as np
+from helper_data import match_options_week1
 
 
-#df = get_historical_data('GE', start = strat, end=end, output_format= 'pandas')
-#print(df.head())
+df_plays = pd.read_csv('dash_plays.csv')
+df_dash_week1 = pd.read_csv('matches_week1_pie.csv')
+#df_pie = df_dash_week1.groupby(['matches', 'possessionTeam', 'passResult'])[['passResult']].count()
+#df_pie = df_pie.add_suffix('_Count').reset_index()
 
-#define my application
-app = dash.Dash()
-
-# define my html component
-app.layout = html.Div(children = [  html.Div("NFL Offense Analytics", 
-                                            style = {"color": "red",
-                                                    "text-align": "center",
-                                                    "border-style" : "dotted",
-                                                    "background-color": "lightblue",
-                                                    "display": "inline-block",
-                                                    "width": "80%"}), 
-                                    # to have a drop down on each college number of players and their role(off/Def)
-                                    html.Div( children=[     
-                                                            html.Div(dcc.Dropdown(id = 'team_roles', options = options_list, value = 'Offense'),
-                                                            style = {   "color": "red",
-                                                                        "text-align": "center",
-                                                                        "border-style" : "dotted",
-                                                                        "background-color": "lightblue",
-                                                                        "display": "inline-block",
-                                                                        "width": "80%"}),
-                                                            html.Div(dcc.Graph(id = 'top_universities', figure = {data = [], layout = []}))
-                                                                            ]),
-                                    # to have a drop down on each role based on offene defense
-                                    html.Div( children = [ html.Div(" Drop Button Off-Def"), 
-                                                            html.Div("role of players")])
-
-    
-])
+app = dash.Dash(__name__)
 
 
+#.................................................HTML LAYOUT.............................................
 
-## creating callbacks
-@app.callback ([
-    output =(
+# 1st row showing pass results of whole game with pie chart and then each team with barchart
+app.layout = html.Div(children = [ 
+    #to create a drop down for picking the week number
+    html.Div([
+        html.Label([' Match analysis']), 
+                                    dcc.Dropdown(
+                                        id = 'week_match',
+                                        options = match_options_week1,
+                                        value = '[PHI , ATL]',
+                                        multi = False,
+                                        clearable = False,
+                                        style = {'width': '50%'}
+                                    )
 
+]), 
+# bellow the drop down of week number present the graph of pass result for the selected match
+html.Div([
+    dcc.Graph(
+        id = 'pie_match_pass_result'
     )
+]),
+
 ])
 
 
+#.......................................................call back functions
+
+@app.callback(
+    Output(component_id = 'pie_match_pass_result', component_property= 'figure'),
+    [Input(component_id='week_match', component_property='value')]
+)
+#define a function that can update the figure that is going to be shown as graph
+def pie_match_pass_fig(week_match):
+
+    #df_new = df_pie.loc[df_pie['matches']== week_match]
+    pasRes_pichart = px.pie(
+    data_frame = df_dash_week1,  values= week_match, color = 'passResult',
+ hole = 0.3)
+
+    return (pasRes_pichart)
 
 
+
+
+
+
+
+
+#..........................................................................................
 if __name__ =='__main__':
     app.run_server(debug=True) 
--- DashAPP/helper_data.py
+++ DashAPP/helper_data.py
@@ -1,8 +1,21 @@
 '''helper data for main app and helper functions'''
 
-options_list =[
-        {'label': 'Defense', 'value': 'Defense'},
-        {'label': 'Offense', 'value': 'Offense'},
-        {'label': 'Special', 'value': 'Special'}
-    ]
-  
+ 
+match_options_week1 = [
+        {'label': '[PHI , ATL]', 'value': '[PHI , ATL]'},
+        {'label': '[CLE , PIT]', 'value': '[CLE , PIT]'},
+        {'label': '[IND , CIN]', 'value': '[IND , CIN]'},
+        {'label': '[MIA , TEN]', 'value': '[MIA , TEN]'},
+        {'label': '[BAL , BUF]', 'value': '[BAL , BUF]'},
+        {'label': '[NE , HOU]', 'value': '[NE , HOU]'},
+        {'label': '[NYG , JAX]', 'value': '[NYG , JAX]'},
+        {'label': '[NO , TB]', 'value': '[NO , TB]'},
+        {'label': '[ARI , WAS]', 'value': '[ARI , WAS]'},
+        {'label': '[CAR , DAL]', 'value': '[CAR , DAL]'},
+        {'label': '[GB , CHI]', 'value': '[GB , CHI]'},
+        {'label': '[DET , NYJ]', 'value': '[DET , NYJ]'},
+        {'label': '[OAK , LA]', 'value': '[OAK , LA]'}
+]
+
+
+
